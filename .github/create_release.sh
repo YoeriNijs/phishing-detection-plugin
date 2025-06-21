905567a2d418a1f7797e2841d2ab49a53f33e3a4
Yoeri Nijs <yoeri@posteo.net>
2025-06-21 21:13:14 +0200

    fix(release): release fix

--- .github/create_release.sh
+++ .github/create_release.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-rm -rf ../release/*.zip
-rm -rf ../release/*.txt
+rm -rf ./release/*.zip
+rm -rf ./release/*.txt
 
-zip -r firefox.zip ../build/firefox
-cp ./firefox.zip ../release/firefox.zip
-sha256sum ../release/firefox.zip > "../release/firefox_$NEW_VERSION.sha256sum.txt"
+zip -r firefox.zip ./build/firefox/
+cp ./firefox.zip ./release/firefox.zip
+sha256sum ./release/firefox.zip > "./release/firefox_$NEW_VERSION.sha256sum.txt"
 echo "Created Firefox release version $NEW_VERSION"
 
-zip -r chrome.zip ../build/chrome
-cp ./chrome.zip ../release/chrome.zip
-sha256sum ../release/chrome.zip > "./release/chrome_$NEW_VERSION.sha256sum.txt"
+zip -r chrome.zip ./build/chrome
+cp ./chrome.zip ./release/chrome.zip
+sha256sum ./release/chrome.zip > "./release/chrome_$NEW_VERSION.sha256sum.txt"
 echo "Created Chrome release version $NEW_VERSION"
 
 
