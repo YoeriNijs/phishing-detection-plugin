2a293f04d83d85378a974f1536df632673c465c6
Yoeri Nijs <yoeri@posteo.net>
2025-06-21 22:11:54 +0200

    fix(rules): removed NU.nl rule (which acted as a test rule for a while)

--- src/rules/nl/index.ts
+++ src/rules/nl/index.ts
@@ -1,14 +1,12 @@
 import { PhishingRules } from '../../model/phishing-rules';
 import { RABOBANK_RULES } from './banks/_rabobank';
 import { ING_RULES } from './banks/_ing';
-import { NU_RULES } from './misc/_nu';
 import { VERIFIEREN_RULES } from './misc/_verifieren';
 import { NL_KEYWORDS_RULES } from './misc/_keywords';
 
 export const DUTCH_BANKS: PhishingRules[] = [RABOBANK_RULES, ING_RULES];
 export const DUTCH_MISC: PhishingRules[] = [
   NL_KEYWORDS_RULES,
-  NU_RULES,
   VERIFIEREN_RULES
 ];
 export const ALL_DUTCH: PhishingRules[] = [...DUTCH_BANKS, ...DUTCH_MISC];
