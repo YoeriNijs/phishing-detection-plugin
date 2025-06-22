da04a1756fd572be02a444309a03b7ccc3566fcb
Yoeri Nijs <yoeri@posteo.net>
2025-06-22 19:50:10 +0200

    feat(rules): add scam page rule

--- src/rules/int/index.ts
+++ src/rules/int/index.ts
@@ -1,4 +1,9 @@
 import { INT_BANK_RULES } from './bank';
 import { INT_COMPANIES } from './companies';
+import { INT_MISC } from './misc';
 
-export const ALL_INTERNATIONAL = [...INT_BANK_RULES, ...INT_COMPANIES];
+export const ALL_INTERNATIONAL = [
+  ...INT_BANK_RULES,
+  ...INT_COMPANIES,
+  ...INT_MISC
+];
