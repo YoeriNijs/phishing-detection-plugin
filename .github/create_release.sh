82cb5a0b091e476ef421c72aecd497ac7c7f1f79
Yoeri Nijs <yoeri@posteo.net>
2025-07-04 23:44:02 +0200

    Update create_release.sh

--- .github/create_release.sh
+++ .github/create_release.sh
@@ -4,22 +4,18 @@ rm -rf ./release/*.crx
 rm -rf ./release/*.zip
 rm -rf ./release/*.txt
 
+echo "Create Firefox development release"
 cd ./build/firefox
-crx pack ../firefox -p ../../plugin.pem -o "../../release/firefox_plugin_$NEW_VERSION.crx"
-sha256sum "../../release/firefox_plugin_$NEW_VERSION.crx" > "../../release/firefox_plugin_$NEW_VERSION.sha256sum.txt"
-echo "Created Firefox crx file"
-
 zip -r firefox.zip .
 cp ./firefox.zip "../../release/firefox_source_$NEW_VERSION.zip"
 sha256sum "../../release/firefox_source_$NEW_VERSION.zip" > "../../release/firefox_source_$NEW_VERSION.sha256sum.txt"
-echo "Created Firefox release version $NEW_VERSION"
 
+echo "Create Chrome crx testing release"
 cd ../chrome
 crx pack ../chrome -p ../../plugin.pem -o "../../release/chrome_plugin_$NEW_VERSION.crx"
 sha256sum "../../release/chrome_plugin_$NEW_VERSION.crx" > "../../release/chrome_plugin_$NEW_VERSION.sha256sum.txt"
-echo "Created Chrome crx file"
 
+echo "Create Chrome development release"
 zip -r chrome.zip .
 cp ./chrome.zip "../../release/chrome_source_$NEW_VERSION.zip"
 sha256sum "../../release/chrome_source_$NEW_VERSION.zip" > "../../release/chrome_source_$NEW_VERSION.sha256sum.txt"
-echo "Created Chrome release version $NEW_VERSION"
