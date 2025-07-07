d376a2340ab902bb8d1d376f93503efc0a5b6758
Yoeri Nijs <yoeri@posteo.net>
2025-07-07 22:15:22 +0200

    refactor(manifest v3): migrating to manifest v3, wip

--- src/browsers/chrome/chrome-plugin.ts
+++ src/browsers/chrome/chrome-plugin.ts
@@ -2,6 +2,13 @@ import { ChromeStorage } from './chrome-storage';
 import { DEFAULT_RULES } from '../../rules/default';
 import { PhishingDetectionPlugin } from '../_shared/plugin';
 
+importScripts('settings.js');
+importScripts('report.js');
+importScripts('unblock.js');
+importScripts('i18n.js');
+
 // Initialize plugin
-const storage = new ChromeStorage(DEFAULT_RULES);
-new PhishingDetectionPlugin(storage, chrome);
+chrome.runtime.onInstalled.addListener(() => {
+  const storage = new ChromeStorage(DEFAULT_RULES);
+  new PhishingDetectionPlugin(storage, chrome);
+});
