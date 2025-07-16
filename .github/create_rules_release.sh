b3cad81dfeb094631d27e6b1efb7bc74b510fd57
Yoeri Nijs <yoeri@posteo.net>
2025-07-16 21:33:12 +0200

    Update create_rules_release.sh

--- .github/create_rules_release.sh
+++ .github/create_rules_release.sh
@@ -3,3 +3,4 @@ echo "Create rules release"
 rm -rf ./release/latest_rules.json
 npm run build:rules
 cp ./release-rules/latest_rules.json ./release
+sha256sum "./release/latest_rules.json" > "./release/latest_rules.sha256sum.txt"
