25e7dbdd84f9f18b228e405e33c7a086a7cb80ea
Yoeri Nijs <yoeri@posteo.net>
2025-07-24 10:10:25 +0200

    feat(rules): dutch ISP rules (#40)

--- src/rules/nl/index.ts
+++ src/rules/nl/index.ts
@@ -1,5 +1,10 @@
 import { PhishingRules } from '../../model/phishing-rules';
 import { DUTCH_BANKS } from './banks';
 import { DUTCH_COMPANIES } from './companies';
+import { DUTCH_ISP } from './isp';
 
-export const ALL_DUTCH: PhishingRules[] = [...DUTCH_BANKS, ...DUTCH_COMPANIES];
+export const ALL_DUTCH: PhishingRules[] = [
+  ...DUTCH_BANKS,
+  ...DUTCH_COMPANIES,
+  ...DUTCH_ISP
+];
---
+++ src/rules/nl/isp/index.ts
@@ -0,0 +1,3 @@
+import { GENERIC_ISP_RULES } from './_generic';
+
+export const DUTCH_ISP = [GENERIC_ISP_RULES];
